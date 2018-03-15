c7c9aebcfb0e59c52074431a89c824cb9a24d333
Trust <haoli@rtigertech.com>
2018-03-15 12:19:32 +0800

    123123123

---
+++ app/src/main/java/com/trust/tnighttalk/RetrofitTool.kt
@@ -0,0 +1,7 @@
+package com.trust.tnighttalk
+
+/**
+ * Created by Trust on 2018/2/8.
+ */
+class RetrofitTool {
+}
---
+++ app/src/main/java/com/trust/tnighttalk/bluetooth/BlueToothActivity.kt
@@ -0,0 +1,12 @@
+package com.trust.tnighttalk.bluetooth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BlueToothActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blue_tooth)
+    }
+}
